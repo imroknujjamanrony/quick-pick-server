040b5eacab5abccff0abd1e94f9bb54fc3bd8ad9
Ssiyam0123 <ssiyam563@gmail.com>
2025-08-06 16:48:15 +0600

    implement user registration with error handling and response structure

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,35 @@
+import { User } from "../model/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const registerUser = async (req, res) => {
-  res.status(200).json({ success: true, requestBy: "siyam" });
+  const { username, fullname, email, password } = req.body;
+  console.log(username);
+
+  if (!username || !fullname || !email || !password)
+    throw new ApiError(400, "all fields are required");
+
+  const isExisted = await User.findOne({ email });
+
+  if (isExisted) throw new ApiError(409, "user allready exist! please login");
+
+  const newUser = await User.create({
+    username,
+    fullname,
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(newUser?._id).select("-password");
+  console.log(createdUser);
+
+  if (!createdUser) throw new ApiError(500, "something went wrong");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "user register successfull"));
 };
+
+
+
 
 export { registerUser };
---
+++ src/model/user.model.js
@@ -0,0 +1,73 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [6, "password must be least 6 characters"],
+  },
+  role: {
+    type: String,
+    default: "CUSTOMER",
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function (id) {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function (id) {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+    },
+    process.env.REFRESSH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
