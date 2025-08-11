8d34f67be4586abcf22a10f6b1a6f61858fd4a84
jyotiprokash <jyotiprokash2000@gmail.com>
2025-08-11 20:42:05 +0600

    create branch jyoti

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const uri =
   process.env.MONGO_URI ||
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zb1tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version //
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
